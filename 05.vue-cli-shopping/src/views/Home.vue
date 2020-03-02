706212fd347aaf651bfc2339dc1fa37cc68ef202
lisdoyle <lisdoyle@126.com>
2020-03-02 17:14:24 +0800

    创建vue-cli-shopping项目

---
+++ 05.vue-cli-shopping/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p class="text">this is text</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+.home{
+  .text{
+    color: red;
+  }
+}
+
+</style>
